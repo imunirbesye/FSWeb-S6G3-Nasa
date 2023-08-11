1a3f2ea3caf3558973f9ee9e93bb3d5c9f952df6
Münir BESYE <imunirbesye@gmail.com>
2023-08-11 23:47:23 +0300

    yapildi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios, {isCancel, AxiosError} from 'axios';
 import "./App.css";
+import LogoCubugu from "./bilesenler/LogoCubugu";
+import Contents from "./bilesenler/Contents"; 
 
 function App() {
+
+  const [veriler, setVeriler] = useState([]); 
+
+  useEffect(
+    () => {
+      const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2023-05-31";
+      axios.get(apiUrl)
+      .then((response) => {    
+        setVeriler(response.data); 
+      })
+      .catch((err) => {
+        console.log(err);
+      }); 
+    }, []);
+
   return (
-    <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="App"> 
+      <LogoCubugu />
+      {
+        veriler ? <Contents veriler={veriler} /> : "Yükleniyor"
+      }
+      
     </div>
   );
 }
 
-export default App;
+export default App; 
