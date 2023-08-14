b47fd2a41f4ec8747ed3a2b9fb3f04083e9e6153
imunirbesye <126553013+imunirbesye@users.noreply.github.com>
2023-08-14 19:39:04 +0300

    Update LogoCubugu.js

--- src/bilesenler/LogoCubugu.js
+++ src/bilesenler/LogoCubugu.js
@@ -1,1 +1,14 @@
+import React, {useState} from "react";
+import "./logocubugu.css";
 
+const LogoCubugu = () => {
+
+
+    return (
+        <div className="logo-cubugu">
+            <img src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"/>
+        </div>
+    )
+};
+
+export default LogoCubugu; 
